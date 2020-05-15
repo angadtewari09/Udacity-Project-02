6a81b3c17b30a8a27e7e2c390fc5a3ec23ba3f18
angadtewari09 <despacito0099@gmail.com>
2020-05-15 22:55:37 +0530

    Minor changes done

--- js/app.js
+++ js/app.js
@@ -7,22 +7,31 @@ navigation.addEventListener('click' , function() {
 
      document.getElementById('navhome').addEventListener('click', (event)=> {
        event.preventDefault();
-       document.getElementById("myhome").scrollIntoView();
+       document.getElementById("homes1").scrollIntoView();
       });
       
       document.getElementById('navabout').addEventListener('click', (e)=> {
         e.preventDefault();
-        document.getElementById("abouts").scrollIntoView();
+        document.getElementById("abouts2").scrollIntoView();
       });
 
       document.getElementById("navhobbies").addEventListener('click',(e)=>{
         e.preventDefault();
-        document.getElementById("hobbies").scrollIntoView();
+        document.getElementById("hobbies3").scrollIntoView();
       });
 
       document.getElementById('navcontact').addEventListener('click', (e)=> {
         e.preventDefault();
-        document.getElementById("contacts").scrollIntoView();
+        document.getElementById("contacts4").scrollIntoView();
       });
       
 }
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
